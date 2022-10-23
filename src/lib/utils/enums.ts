2c648a687e71f402d0dfb0d16f1edd31d3c3c2f2
Aryan Agarwal <aryankush025@gmail.com>
2022-10-24 03:19:06 +0530

    Basic enums added

---
+++ src/lib/utils/enums.ts
@@ -0,0 +1,14 @@
+export enum AsyncStates {
+	initial,
+	inProgress,
+	success,
+	error
+}
+
+export enum ApiRequestMethods {
+	get = 'GET',
+	post = 'POST',
+	patch = 'PATCH',
+	delete = 'DELETE',
+	put = 'PUT'
+}
