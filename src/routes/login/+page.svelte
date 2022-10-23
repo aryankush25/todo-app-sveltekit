02a161d608f07a2d6d6109314faa7b6c5b8d15d8
Aryan Agarwal <aryankush025@gmail.com>
2022-10-23 15:33:04 +0530

    Basic login form added

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	let name = '';
+	let password = '';
+
+	function handleSubmit(event: SubmitEvent) {
+		console.log('Value', event);
+		console.log('event.target', event.target);
+		console.log({ name, password });
+	}
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+	<div class="pb">
+		<label for="name">Full Name:</label><br />
+		<input type="text" id="name" name="name" bind:value={name} /><br />
+	</div>
+
+	<div class="pb">
+		<label for="password">Password:</label><br />
+		<input type="password" id="password" name="password" bind:value={password} /><br />
+	</div>
+
+	<input type="submit" value="Submit" />
+</form>
+
+<style>
+	form {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	.pb {
+		padding-bottom: 12px;
+	}
+</style>
