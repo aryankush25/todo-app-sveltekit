7ab8cd51ab9ac858f0de64a05a54e1408a870bdd
Aryan Agarwal <aryankush025@gmail.com>
2022-10-31 02:02:44 +0530

    Conditional Tailwind classes added - Not a good approach as we have to again and again repeat same classes

--- src/routes/(unauthenticated)/login/+page.svelte
+++ src/routes/(unauthenticated)/login/+page.svelte
@@ -57,5 +57,8 @@
 		class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-primary hover:text-white focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
 		value={apiState === AsyncStates.inProgress ? 'Logging in...' : 'Log in'}
 		disabled={apiState === AsyncStates.inProgress}
+		class:cursor-not-allowed={apiState === AsyncStates.inProgress}
+		class:bg-primary={apiState === AsyncStates.inProgress}
+		class:text-white={apiState === AsyncStates.inProgress}
 	/>
 </form>
