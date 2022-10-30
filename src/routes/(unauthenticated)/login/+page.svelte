6cefb9ec8cf1fd51ee52d43c0da97c884fd0f6e8
Aryan Agarwal <aryankush025@gmail.com>
2022-10-31 01:54:01 +0530

    Tailwind CSS added to login page form

--- src/routes/(unauthenticated)/login/+page.svelte
+++ src/routes/(unauthenticated)/login/+page.svelte
@@ -21,45 +21,41 @@
 	};
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-	<div class="pb">
-		<label for="email">Email:</label><br />
-		<input
-			type="text"
-			id="email"
-			name="email"
-			bind:value={email}
-			disabled={apiState === AsyncStates.inProgress}
-		/><br />
+<form on:submit|preventDefault={handleSubmit} class="flex justify-center flex-col items-center">
+	<div class="pb-3.5">
+		<label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+
+		<div class="relative mt-1 rounded-md shadow-sm">
+			<input
+				type="text"
+				id="email"
+				name="email"
+				class="block w-full rounded-md border-gray-300 px-5 focus:border-primary focus:ring-primary sm:text-sm"
+				bind:value={email}
+				disabled={apiState === AsyncStates.inProgress}
+			/>
+		</div>
 	</div>
 
-	<div class="pb">
-		<label for="password">Password:</label><br />
-		<input
-			type="password"
-			id="password"
-			name="password"
-			bind:value={password}
-			disabled={apiState === AsyncStates.inProgress}
-		/><br />
+	<div class="pb-3.5">
+		<label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+
+		<div class="relative mt-1 rounded-md shadow-sm">
+			<input
+				type="text"
+				id="password"
+				name="password"
+				class="block w-full rounded-md border-gray-300 px-5 focus:border-primary focus:ring-primary sm:text-sm"
+				bind:value={password}
+				disabled={apiState === AsyncStates.inProgress}
+			/>
+		</div>
 	</div>
 
 	<input
 		type="submit"
+		class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-primary hover:text-white focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
 		value={apiState === AsyncStates.inProgress ? 'Logging in...' : 'Log in'}
 		disabled={apiState === AsyncStates.inProgress}
 	/>
 </form>
-
-<style>
-	form {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-	}
-
-	.pb {
-		padding-bottom: 12px;
-	}
-</style>
