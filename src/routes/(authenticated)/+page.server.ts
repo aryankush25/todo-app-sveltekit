9ec8ae8e2a521ab7fbc8624f7c339e85a5736dc1
Aryan Agarwal <aryankush025@gmail.com>
2022-11-01 02:08:53 +0530

    Tasks list fetched on / route

--- src/routes/(authenticated)/+page.server.ts
+++ src/routes/(authenticated)/+page.server.ts
@@ -1,11 +1,18 @@
 import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 import { isNilOrEmpty } from '$lib/utils/helpers';
+import tasksApi from '$lib/services/modules/tasksApi';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (isNilOrEmpty(locals.accessToken)) {
 		throw redirect(307, '/login');
 	}
 
-	return {};
+	const getTasksResponse = await tasksApi.getTasks(locals.accessToken);
+
+	if (!getTasksResponse.success) {
+		throw getTasksResponse.error;
+	}
+
+	return { tasksList: getTasksResponse.data };
 };
