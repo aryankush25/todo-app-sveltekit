e5e9a9278c2ba324c4f4e4f4c7553ac10af809a2
Aryan Agarwal <aryankush025@gmail.com>
2022-11-01 02:37:33 +0530

    / route layout changes

--- src/routes/(authenticated)/+page.svelte
+++ src/routes/(authenticated)/+page.svelte
@@ -1,7 +1,10 @@
 <script>
+	import { page } from '$app/stores';
 	import Counter from '$lib/components/Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
+	$: console.log($page.data.tasksList);
 </script>
 
 <svelte:head>
@@ -9,51 +12,23 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
+<section class="flex flex-col justify-center items-center">
+	<h1 class="w-full">
+		<span class="welcome w-full block h-0 relative">
 			<picture>
 				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
+				<img src={welcome_fallback} alt="Welcome" class="absolute w-full h-full top-0 block" />
 			</picture>
 		</span>
-
-		to your new<br />SvelteKit app
 	</h1>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+	<h2>Wana play? Try our counter.</h2>
 
 	<Counter />
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
 	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
 		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
