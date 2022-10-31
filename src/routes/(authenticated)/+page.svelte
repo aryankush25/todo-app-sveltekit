5de8d7d1f38ca76b275ae6c91b1f03997d3821cc
Aryan Agarwal <aryankush025@gmail.com>
2022-11-01 02:53:11 +0530

    Tasks lists rendered

--- src/routes/(authenticated)/+page.svelte
+++ src/routes/(authenticated)/+page.svelte
@@ -22,6 +22,27 @@
 		</span>
 	</h1>
 
+	<div class="table w-6/12 mb-12">
+		<div class="table-header-group">
+			<div class="table-row">
+				<div class="table-cell text-center">Completed</div>
+				<div class="table-cell text-center">Task</div>
+			</div>
+		</div>
+
+		<div class="table-row-group">
+			{#each $page.data.tasksList as task (task._id)}
+				<div class="table-row">
+					<div class="table-cell text-center">
+						<input type="checkbox" class="default:ring-2" checked={task.completed} disabled />
+					</div>
+
+					<div class="table-cell text-center">{task.description}</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+
 	<h2>Wana play? Try our counter.</h2>
 
 	<Counter />
