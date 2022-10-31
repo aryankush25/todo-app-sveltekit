9b369e084b526464297c209b10365cb560159608
Aryan Agarwal <aryankush025@gmail.com>
2022-11-01 02:18:58 +0530

    wait function added

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -2,3 +2,7 @@ import * as R from 'ramda';
 
 export const isNilOrEmpty = R.anyPass([R.isNil, R.isEmpty]);
 export const isPresent = R.complement(isNilOrEmpty);
+export const wait = (ms: number) =>
+	new Promise((resolve) => {
+		setTimeout(() => resolve('Resolved'), ms);
+	});
